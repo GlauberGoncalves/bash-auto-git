3b4b622060883a85fd61650113bc1941cca2ec05
Glauber Gonçalves <sistemas.glauber@gmail.com>
2018-06-20 16:03:37 -0300

    first commit

---
+++ glauber.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+
+echo "informe a mensagem"
+git config --global user.name "GlauberGoncalves"
+git config --global user.email "sistemas.glauber@gmail.com"
+git add .
+git commit -m 'teste'
+git push
